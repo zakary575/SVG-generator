6c01bba5a8727e56fe317679c69b4cc1cc6dd088
Zakary Johnson <zakary.johnson575@gmail.com>
2024-09-08 18:05:19 -0500

    creaed the functions to generate the SVG

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 const inquirer = require("inquirer");
+const MaxLengthInputPrompt = require("inquirer-maxlength-input-prompt");
 
-
-
+inquirer.registerPrompt("maxlength-input", MaxLengthInputPrompt);
 
 inquirer
-.prompt([
-        {
-            type: "input-maxlength",
-            name: "text",
-            message: "Enter up to three characters",
-            maxlegth: 3,
-          },
-          {
-            type: "input",
-            name: "text-color",
-            message: "Enter a color for your text",
-          },
-          {
-            type: "list",
-            name: "shape",
-            message: "Pick a shape",
-            choices: ['Circle',"Triangle","Square"],
+  .prompt([
+    {
+      type: "maxlength-input",
+      name: "text",
+      message: "Enter up to three characters",
+      maxLength: 3,
+    },
+    {
+      type: "input",
+      name: "textColor",
+      message: "Enter a color for your text",
+    },
+    {
+      type: "list",
+      name: "shape",
+      message: "Pick a shape",
+      choices: ["Circle", "Triangle", "Square"],
+    },
+    {
+      type: "input",
+      name: "shapeColor",
+      message: "Enter a shape color",
+    },
+  ])
+  .then((answers) => {
+    console.log(answers);
+  });
 
-          },
-          {
-            type: "input",
-            name: "shape-color",
-            message: "Enter a shape color",
-          },
-]
-)
