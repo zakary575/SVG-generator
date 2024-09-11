525688bc38080abc5f3937ef21a81eae6a483b70
Zakary Johnson <zakary.johnson575@gmail.com>
2024-09-11 00:17:09 -0500

    added test and changed the class to not a constructor so that every thing is set with the set functions

--- index.js
+++ index.js
@@ -35,29 +35,22 @@ inquirer
     let newShape;
     switch (answers.shape) {
       case "Circle":
-        newShape = new shapes.Circle(
-          {text:answers.text,textColor:answers.textColor,shapeColor:answers.shapeColor}
-        )
+        newShape = new shapes.Circle();
         break;
       case "Triangle":
-        newShape = new shapes.Triangle(
-          {text:answers.text,textColor:answers.textColor,shapeColor:answers.shapeColor}
-        )
+        newShape = new shapes.Triangle();
         break;
       case "Square":
-        newShape = new shapes.Square(
-          {text:answers.text,textColor:answers.textColor,shapeColor:answers.shapeColor}
-        )
+        newShape = new shapes.Square();
         break;
       default:
         console.log("Shape not implmented");
     }
-  
-    newShape.setColor(answers.shapeColor)
-    newShape.setText(answers.text)
-    newShape.setTextColor(answers.textColor)
 
-    const svg = newShape.renderSVG()
+    newShape.setColor(answers.shapeColor);
+    newShape.setText(answers.text, answers.textColor);
+
+    const svg = newShape.renderSVG();
 
     fs.writeFile("logo.svg", svg, (err) =>
       err ? console.log(err) : console.log("Generated logo.svg")
